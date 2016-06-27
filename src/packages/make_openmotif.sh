53a10c29b460cc7a4dd8a770bfb0ae4fc7dd4b09
Kjetil Matheussen <k.s.matheussen@notam02.no>
2016-06-27 10:02:38 +0200

    Fix openmotif compilation

--- src/packages/make_openmotif.sh
+++ src/packages/make_openmotif.sh
@@ -4,11 +4,13 @@ rm -fr openmotif-2.3.2
 tar xvzf openmotif-2.3.2.tar.gz
 cd openmotif-2.3.2
 
+echo "$(echo '%option main' | cat - tools/wml/wmluiltok.l)" > tools/wml/wmluiltok.l
+
 export CFLAGS="$CFLAGS -fPIC"
 export LDFLAGS="$LDFLAGS -fPIC"
 
 unset LANG
-if ./configure --prefix=$1 --with-pic && make ; then
+if ./configure --prefix=$1 --with-pic && make -j8 ; then
     cd ..
     touch openmotif_compiled
 else
